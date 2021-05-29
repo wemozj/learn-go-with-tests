214d14b3af652eb8236613eb36d42b1e93adc933
wemozj <1348182282@qq.com>
2021-05-29 17:33:30 +0800

    arrays test

---
+++ arrays/sum_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if want != got {
+		t.Errorf("gived %v, got %d, but expected %d", numbers, got, want)
+	}
+}
