c8fb84fe95b71ca2c8036180bdb7ac98596f8d40
wemozj <1348182282@qq.com>
2021-05-29 17:56:55 +0800

    SumAll test success

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -25,5 +28,14 @@ func TestSum(t *testing.T) {
 			t.Errorf("gived %v, got %d, but expected %d", numbers, got, want)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1,2}, []int{0,9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
