9dcc3dffdc0bf0f6e31c77a51030703c987ae8b1
wemozj <1348182282@qq.com>
2021-05-29 15:07:06 +0800

    add empty string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("wemo")
-	want := "Hello, wemo"
 
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // 测试失败时报告所在的行，而不是辅助函数内部的行
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
