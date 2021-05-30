c3e838a6555c5287826f1affc57a03e3c29ccd94
wemozj <1348182282@qq.com>
2021-05-30 14:22:40 +0800

    pointer test success

---
+++ pointers/wallet.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	//fmt.Println("address of balance in Deposit is ", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
