81dc13c193a6fdcdd33b7ce68bb64ee8474d87fc
wemozj <1348182282@qq.com>
2021-06-08 10:08:49 +0800

    refactor dictionary_test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -2,13 +2,19 @@ package main
 
 import "testing"
 
+func AssertString(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("want %s, but got %s", want, got)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
-	if got != want {
-		t.Errorf("want %s, but got %s", want, got)
-	}
+	AssertString(t, got, want)
 }
