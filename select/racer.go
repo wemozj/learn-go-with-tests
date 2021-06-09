51497f325bbc0039276f5d0d4484b927d52b13b6
wemozj <1348182282@qq.com>
2021-06-09 12:58:05 +0800

    select

---
+++ select/racer.go
@@ -0,0 +1,40 @@
+package racer
+
+import (
+	"net/http"
+)
+
+//func Racer(a, b string) (winner string) {
+//	aDuration := measureResponseTime(a)
+//	bDuration := measureResponseTime(b)
+//
+//	if aDuration < bDuration {
+//		return a
+//	}
+//
+//	return b
+//}
+//
+//func measureResponseTime(url string) time.Duration {
+//	start := time.Now()
+//	http.Get(url)
+//	return time.Since(start)
+//}
+
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
+}
+
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
+}
